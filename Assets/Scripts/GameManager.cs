f2bae530b866aaf20b8aff09efafb8a27dd9b9d4
Lasse Deleuran <lassedeleuran@gmail.com>
2016-04-09 17:09:39 +0200

    Added GM.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,46 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.UI; // Access text elements
+using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
+    public float resetDelay = 3; // Time in secs after game ended.
+    public Text scoreText, highScoreText, altitudeText;
+    public GameObject gameOver;
+    public Airship airshipPrefab;
+    public static GameManager instance = null; // Use instance of this class.
+    private Airship cloneAirship;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+        if (instance == null)
+            instance = this; // Only one GM.
+        else if (instance != this)
+            Destroy(instance); // Prevent multiple GMs when additional scenes are added.
+        Setup();
+    }
+
+    public void Setup()
+    {
+        cloneAirship = (Airship)Instantiate(airshipPrefab, transform.position, Quaternion.identity); // position => identity position at start. identity => no rotation.
+        Instantiate(airshipPrefab, transform.position, Quaternion.identity); // Not accessing them.
+    }
+
+    public void CheckGameOver()
+    {
+        if (cloneAirship.altitudeInMeters <= 0)
+        {
+            gameOver.SetActive(true);
+            Time.timeScale = 0.25f; // slowmo.
+            Invoke("Reset", resetDelay); // Reset the game.
+        }
+    }
+
+    private void Reset()
+    {
+        Time.timeScale = 1;
+        //Application.LoadLevel(Application.loadedLevel); // Load scene. Reload last loaded scene.
+        SceneManager.LoadScene(0); // Load scene. Reload last loaded scene.
+    }
 }
